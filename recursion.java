1670848c7d3676f08e6e437e2a8f62a8f447dad2
yiqibyte <yiqi.h@outlook.com>
2025-07-08 16:12:28 +0800

    07-08

--- recursion.java
+++ recursion.java
@@ -1,30 +1,31 @@
-// package DSAA;
+import java.math.BigInteger;
 
 public class recursion {
-	public static void main(String[] args) {
-		int n = 10;
-		long startTime1 = System.nanoTime();
-		int res1 = factorial(n);
-		System.out.println(res1);
-		long endTime1 = System.nanoTime();
-		System.out.println(endTime1 - startTime1);
-		
-		long startTime2 = System.nanoTime();
-		int res2 = tail_factorial(n, 1);
-		System.out.println(res2);
-		long endTime2 = System.nanoTime();
-		System.out.println(endTime2 - startTime2);
+    public static void main(String[] args) {
+        int n = 100;
 
+        long startTime1 = System.nanoTime();
+        BigInteger res1 = factorial(BigInteger.valueOf(n));
+        System.out.println(res1);
+        long endTime1 = System.nanoTime();
+        System.out.println("普通递归时间(ns): " + (endTime1 - startTime1));
 
-	}
-	
-	public static int factorial(int n) {
-		if (n == 1) return 1;
-		return n * factorial(n - 1);
-	}
+        long startTime2 = System.nanoTime();
+        BigInteger res2 = tailFactorial(BigInteger.valueOf(n), BigInteger.ONE);
+        System.out.println(res2);
+        long endTime2 = System.nanoTime();
+        System.out.println("尾递归时间(ns): " + (endTime2 - startTime2));
+    }
 
-	public static int tail_factorial(int n, int res) {
-		if (n == 1) return res;
-		return tail_factorial(n-1, res * n);
-	}
+    // 普通递归
+    public static BigInteger factorial(BigInteger n) {
+        if (n.equals(BigInteger.ONE)) return BigInteger.ONE;
+        return n.multiply(factorial(n.subtract(BigInteger.ONE)));
+    }
+
+    // 尾递归版本
+    public static BigInteger tailFactorial(BigInteger n, BigInteger res) {
+        if (n.equals(BigInteger.ONE)) return res;
+        return tailFactorial(n.subtract(BigInteger.ONE), res.multiply(n));
+    }
 }
